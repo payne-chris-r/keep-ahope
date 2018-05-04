3271714d56fe4beecf27c5b0b06c33354b620f55
Camden Hill <camden.hill@ge.com>
2018-05-04 13:22:00 -0400

    WIP--show events and submit intake form

--- src/components/view/ContactInfo/index.js
+++ src/components/view/ContactInfo/index.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import { getImageForEnv } from 'static/images/index';
 import ContactProfileCard from 'components/view/Contact/ContactProfileCard';
 import ContactNotesCard from 'components/view/Contact/ContactNotesCard';
-// import EventsCard from '';
+import ContactEvents from 'components/view/ContactEvents';
 import { cyan600 } from 'material-ui/styles/colors'
 
 class ContactInfo extends Component {
@@ -11,6 +11,22 @@ class ContactInfo extends Component {
     render() {
 
         const { user, uid, contact } = this.props;
+        // console.log(this.props)
+        // let { user, uid, contact } = this.props;
+        // contact.events = [
+        //   {
+        //     uid: "1",
+        //     date: "04-20-2018"
+        //   },
+        //   {
+        //     uid: "1",
+        //     date: "04-12-2018"
+        //   },
+        //   {
+        //     uid: "1",
+        //     date: "03-10-2018"
+        //   },
+        // ]
 
         return (
             <div>
@@ -21,7 +37,7 @@ class ContactInfo extends Component {
 
                 <div>
                     <ContactProfileCard titleColor={cyan600} contact={contact} />
-                    {/* <CamdenEventCard titleColor={cyan600} events={contact.events} /> */}
+                    <ContactEvents titleColor={cyan600} contactEvents={contact.events} />
                     <ContactNotesCard titleColor={cyan600} />
                 </div>
 
